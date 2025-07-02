34b4a59058c84f0717ae915b61f2b3d77cf9aa18
escarsel <eloisa.scarsella@nttdata.com>
2025-07-02 19:56:52 +0200

    rollback config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(() => {
   return {
-    build: {
-      outDir: 'build',
-    },
+    // build: {
+    //   outDir: 'build',
+    // },
     plugins: [react()],
   };
 });
