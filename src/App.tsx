02a552aabcaf00be8bf1ab3d9fe278648bd7c74b
Mattia Laurenzana <mattia.laurenzana@beije.it>
2025-07-02 11:05:22 +0200

    EBE-6 setup keycloak login and base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
 
+// //components
+import Layout from './components/Layout/Layout';
+
+
 function App() {
 
   return (
-    <div>
-      <h1 style={{ textAlign: 'center' }}>Portale operatore punto vendita</h1>
-      <p style={{ textAlign: 'center' }}>Web portal to serve the operators of the single point of sale in the idpay's ecosystem</p>
-    </div>
+    <Layout>
+    </Layout>
   )
 }
 
 export default App
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { ReactKeycloakProvider } from '@react-keycloak/web';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 import keycloak from './keycloak/keycloak';
-
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from '@pagopa/mui-italia';
+import { KeycloakProvider } from "keycloak-react-web";
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-    <ReactKeycloakProvider authClient={keycloak}  initOptions={{ onLoad: 'login-required', checkLoginIframe: false,
-        redirectUri: 'http://localhost:5173' }}  >
-    <App />
-    </ReactKeycloakProvider>
-)
+  <KeycloakProvider client={keycloak} initOptions={{
+    onLoad: 'login-required', checkLoginIframe: false,
+    redirectUri: 'http://localhost:5173'
+  }}>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </KeycloakProvider>)
+
