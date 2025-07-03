16519dab27845bb80623c5757a2a8e503c1e4ac2
mattialaurenzana21 <Mattia.Laurenzana@nttdata.com>
2025-07-03 16:22:26 +0200

    Fix: [EBE-6] Updated keycloak-js version (#6)
    * create .gitignore file
    
    * Updated gitignore file
    
    * add file .yml
    
    * EBE-6 setup keycloak login and base layout
    
    * Update .env.development
    
    * EBE-6 updated package json file
    
    * EBE-6 fixed dependency
    
    * EBE-6 fixed pipeline parameter
    
    * Fix build
    
    * update tsconfig
    
    * rollback config
    
    * Fix config for build
    
    * EBE-6 temporary fixed library
    
    * Updated library
    
    * Updated pipeline file
    
    * Updated env file
    
    ---------
    
    Co-authored-by: Mattia Laurenzana <mattia.laurenzana@beije.it>
    Co-authored-by: Christian Geovese <cgenoves@emeal.nttdata.com>
    Co-authored-by: escarsel <eloisa.scarsella@nttdata.com>
    Co-authored-by: angelosca24 <angelo.scagliola@nttdata.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,31 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import keycloak from './keycloak/keycloak';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from '@pagopa/mui-italia';
-import { KeycloakProvider } from "keycloak-react-web";
 import { BrowserRouter } from 'react-router-dom';
 
+import { AuthProvider } from "react-oidc-context";
+
+
+const oidcConfig = {
+  authority: import.meta.env.VITE_OIDC_AUTHORITY, //provider url
+  client_id: import.meta.env.VITE_OIDC_CLIENT_ID, //client univoque id
+  redirect_uri: import.meta.env.VITE_OIDC_REDIRECT_URI, //redirect uri after a successful login
+  automaticSilentRenew: true, // automatically renews tokens
+  onSigninCallback: () => {
+    // clean the url after a successful login
+    window.history.replaceState({}, document.title, window.location.pathname);
+  },
+};
+
 createRoot(document.getElementById('root')!).render(
-  <KeycloakProvider client={keycloak} initOptions={{
-    onLoad: 'login-required', checkLoginIframe: false,
-    redirectUri: import.meta.env.VITE_REDIRECT_URI
-  }}>
+  <AuthProvider {...oidcConfig}>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <App />
       </ThemeProvider>
     </BrowserRouter>
-  </KeycloakProvider>)
-
+  </AuthProvider>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portale-esercenti/', // <-- importante per il deploy su un path
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+});
