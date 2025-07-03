8a46d0781eaa8a95fe28be886d5708f6d65bb6ed
Mattia Laurenzana <mattia.laurenzana@beije.it>
2025-07-03 15:41:09 +0200

    Updated library

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,31 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import keycloak from './keycloak/keycloak';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from '@pagopa/mui-italia';
-import { ReactKeycloakProvider } from "@react-keycloak/web";
 import { BrowserRouter } from 'react-router-dom';
 
+import { AuthProvider } from "react-oidc-context";
+
+
+const oidcConfig = {
+  authority: import.meta.env.VITE_OIDC_AUTHORITY, //provider url
+  client_id: import.meta.env.VITE_OIDC_CLIENT_ID, //client univoque id
+  redirect_uri: import.meta.env.VITE_OIDC_REDIRECT_URI, //redirect uri after a successful login
+  automaticSilentRenew: true, // automatically renews tokens
+  onSigninCallback: () => {
+    // clean the url after a successful login
+    window.history.replaceState({}, document.title, window.location.pathname);
+  },
+};
+
 createRoot(document.getElementById('root')!).render(
-  <ReactKeycloakProvider authClient={keycloak} initOptions={{
-    onLoad: 'login-required', checkLoginIframe: false
-  }}>
+  <AuthProvider {...oidcConfig}>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <App />
       </ThemeProvider>
     </BrowserRouter>
-  </ReactKeycloakProvider>)
-
+  </AuthProvider>
+);
