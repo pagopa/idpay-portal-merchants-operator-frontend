536014a19a5b3249261872a3ed3b585727366ee7
Mattia Laurenzana <mattia.laurenzana@beije.it>
2025-07-15 15:17:07 +0200

    [EBE-6-fix]: added test path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/esercente/', // <-- importante per il deploy su un path
+  base: '/portale-esercenti-test/', // <-- importante per il deploy su un path
   plugins: [react()],
   build: {
     outDir: 'dist',
