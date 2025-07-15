e62164a7a68d59592daa742189d0a3e56419aa25
Mattia Laurenzana <mattia.laurenzana@beije.it>
2025-07-15 14:29:10 +0200

    [EBE-6-fix]: fix path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portale-esercenti/', // <-- importante per il deploy su un path
+  base: '/esercente/', // <-- importante per il deploy su un path
   plugins: [react()],
   build: {
     outDir: 'dist',
