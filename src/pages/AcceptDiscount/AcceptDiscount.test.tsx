42a421c438a7281383657dce29f4f0973d30151c
Mattia Laurenzana <mattia.laurenzana@beije.it>
2025-08-26 17:48:13 +0200

    [EBE-55]: Added unit test files, acceptDiscountCard and acceptDiscount

---
+++ src/pages/AcceptDiscount/AcceptDiscount.test.tsx
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import '@testing-library/jest-dom';
+// import AcceptDiscount from './AcceptDiscount';
+
+// test('renders without crashing', () => {
+//   render(<AcceptDiscount />);
+// });
+
