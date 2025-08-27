af76a7b8ae6bf87291f1b433482200d330c942a7
and-mora <andrea.morabito@pagopa.it>
2025-08-27 13:02:44 +0200

    add github action for code review

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, mergeConfig } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/esercente/', // <-- importante per il deploy su un path
+const viteConfig = defineConfig({
   plugins: [react()],
+  base: '/esercente/',
   build: {
     outDir: 'dist',
     sourcemap: true,
   },
 });
 
+const vitestConfig = defineVitestConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+      thresholds: {
+        lines: 80,
+        branches: 80,
+        functions: 80,
+        statements: 80
+      }
+    }
+  },
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
