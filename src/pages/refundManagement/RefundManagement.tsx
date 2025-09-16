cde54383c14290827e0a8e6bc2791fe81ae77838
Emanuele-Stazzi <Emanuele11.Stazzi@nttdata.com>
2025-09-16 10:55:52 +0200

    fix: changed label name GTIN (#34)
    Co-authored-by: Emanuele Stazzi <>

--- src/pages/refundManagement/RefundManagement.tsx
+++ src/pages/refundManagement/RefundManagement.tsx
@@ -280,7 +280,7 @@ const RefundManagement = () => {
                     <Grid size={{ xs: 12, sm: 6, md: 3, lg: 2 }}>
                         <TextField
                             name="gtiIn"
-                            label="Cerca per GTI In"
+                            label="Cerca per GTIN"
                             size="small"
                             value={formik.values.gtiIn}
                             onChange={formik.handleChange}
