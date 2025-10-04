b5b6d52577da8cb48fbfd3811700b0afde80dbb8
Mattia Laurenzana <mattia.laurenzana@beije.it>
2025-10-04 11:04:05 +0200

    [EBE-45]: fixed button style

--- src/pages/refund/Refund.tsx
+++ src/pages/refund/Refund.tsx
@@ -38,7 +38,7 @@ const Refund = () => {
                 <Typography variant="body2" mt={4} mb={1}>Carica il file della fattura elettronica relativa a questo acquisto. Assicurati che il documento sia in formato .xml o .pdf e conforme agli standard della fatturazione elettronica</Typography>
                 <Link href="#" sx={{ fontWeight: theme.typography.fontWeightMedium, fontSize: '14px' }}>Dubbi? Vai sul manuale</Link>
                 <SingleFileInput  onFileSelected={handleSelect} onFileRemoved={handleRemove} value={file} dropzoneLabel="Trascina qui il file <PDF> della fattura da caricare o " dropzoneButton="selezionalo dal tuo computer" rejectedLabel="File type not supported" />
-                <Button onClick={handleRemove} sx={{padding: '0'}} color='primary'> <FileUpload /> Sostituisci</Button>
+                <Button variant='naked' onClick={handleRemove} sx={{fontSize: '14px', fontWeight: theme.typography.fontWeightMedium}} color='primary'> <FileUpload /> Sostituisci</Button>
             </Box>
             <Stack direction="row" spacing={2} mt={3} justifyContent="space-between">
                 <Button onClick={handleNavigateBack} variant="outlined">Indietro</Button>
