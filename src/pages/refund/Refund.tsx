5864ed0688332f8ab26a0e4da3a69fd30fd02558
Mattia Laurenzana <mattia.laurenzana@beije.it>
2025-10-04 11:11:05 +0200

    [EBE-45]: added type check

--- src/pages/refund/Refund.tsx
+++ src/pages/refund/Refund.tsx
@@ -5,26 +5,34 @@ import { TitleBox } from '@pagopa/selfcare-common-frontend/lib';
 import { useNavigate } from 'react-router-dom';
 import ROUTES from '../../routes';
 import { theme } from '@pagopa/mui-italia';
-import {SingleFileInput} from '@pagopa/mui-italia';
-import {useState} from 'react';
+import { SingleFileInput } from '@pagopa/mui-italia';
+import { useState, useRef } from 'react';
 import style from './style.module.css'
-import { FileUpload } from '@mui/icons-material';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
 
 const Refund = () => {
     const [file, setFile] = useState<File | null>(null);
     const { t } = useTranslation();
     const navigate = useNavigate();
+    const fileInputRef = useRef<HTMLInputElement>(null);
 
-    const handleSelect = (file: File) => setFile(file)
     const handleRemove = () => setFile(null)
 
     const handleRefund = () => file && navigate(ROUTES.REFUNDS_MANAGEMENT)
     const handleNavigateBack = () => navigate(ROUTES.BUY_MANAGEMENT)
-    
+
+    const handleButtonClick = () => {
+        fileInputRef.current?.click();
+    };
+
+    const handleFileSelect = (file: File) => {
+        setFile(file);
+    };
+
     return (
         <Box>
             <BreadcrumbsBox
-                backLabel={t('commons.exitBtn')} items={['Gestione acquisti', 'Storna transazione']} active={true} onClickBackButton={() => navigate(ROUTES.BUY_MANAGEMENT)} />
+                backLabel={t('commons.exitBtn')} items={['Gestione acquisti', 'Richiedi rimborso']} active={true} onClickBackButton={() => navigate(ROUTES.BUY_MANAGEMENT)} />
             <TitleBox
                 title={t('pages.refund.title')}
                 mtTitle={3}
@@ -33,12 +41,38 @@ const Refund = () => {
                 variantSubTitle='body2'
             />
 
-            <Box sx={{ backgroundColor: theme.palette.background.paper, borderRadius: '4px' }} mt={4} p={2} className={style.uploadFileContainer } >
+            <Box sx={{ backgroundColor: theme.palette.background.paper, borderRadius: '4px' }} mt={4} p={2} className={style.uploadFileContainer} >
                 <Typography variant="h6" fontWeight={theme.typography.fontWeightBold}>Carica la fattura</Typography>
                 <Typography variant="body2" mt={4} mb={1}>Carica il file della fattura elettronica relativa a questo acquisto. Assicurati che il documento sia in formato .xml o .pdf e conforme agli standard della fatturazione elettronica</Typography>
                 <Link href="#" sx={{ fontWeight: theme.typography.fontWeightMedium, fontSize: '14px' }}>Dubbi? Vai sul manuale</Link>
-                <SingleFileInput  onFileSelected={handleSelect} onFileRemoved={handleRemove} value={file} dropzoneLabel="Trascina qui il file <PDF> della fattura da caricare o " dropzoneButton="selezionalo dal tuo computer" rejectedLabel="File type not supported" />
-                <Button variant='naked' onClick={handleRemove} sx={{fontSize: '14px', fontWeight: theme.typography.fontWeightMedium}} color='primary'> <FileUpload /> Sostituisci</Button>
+                <Box mb={2}>
+                    <SingleFileInput accept={['application/pdf', 'application/xml']} onFileSelected={handleFileSelect} onFileRemoved={handleRemove} value={file} dropzoneLabel="Trascina qui il file <PDF> della fattura da caricare o " dropzoneButton="selezionalo dal tuo computer" rejectedLabel="File type not supported" />
+                </Box>
+                <input
+                    type="file"
+                    accept='application/pdf, application/xml'
+                    ref={fileInputRef}
+                    style={{ display: 'none' }}
+                    onChange={(e) => {
+                        const selectedFile = e.target.files?.[0];
+                        if (selectedFile) {
+                            handleFileSelect(selectedFile);
+                        }
+                    }}
+                />
+
+                {
+                    file && (
+                        <Button
+                            variant="naked"
+                            startIcon={<FileUploadIcon />}
+                            onClick={handleButtonClick}
+                            sx={{ fontWeight: 'bold', fontSize: '14px' }}
+                        >
+                            {t('pages.refund.replaceFile')}
+                        </Button>
+                    )
+                }
             </Box>
             <Stack direction="row" spacing={2} mt={3} justifyContent="space-between">
                 <Button onClick={handleNavigateBack} variant="outlined">Indietro</Button>
