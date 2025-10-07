5eaa06113c7f6b29a44ff15b5abc281a37142f66
Mattia Laurenzana <mattia.laurenzana@beije.it>
2025-10-07 12:46:35 +0200

    [EBE-75]: Fixed route

--- src/routes.ts
+++ src/routes.ts
@@ -8,7 +8,7 @@ const ROUTES = {
     BUY_MANAGEMENT: '/gestione-acquisti',
     REFUNDS_MANAGEMENT:'/gestione-rimborsi',
     PROFILE:'/profilo',
-    REVERSE: '/storna-transazione',
+    REVERSE: '/storna-transazione/:trxId',
     REFUND: '/richiedi-rimborso/:trxId'
 };
 
