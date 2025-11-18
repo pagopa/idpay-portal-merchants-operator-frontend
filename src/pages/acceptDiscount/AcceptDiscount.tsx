665f761f4f09de60fe708b65429ec14c563dc42e
Eleonora <cossu.eleonora1996@gmail.com>
2025-11-18 16:12:52 +0100

    fix: [EBE-442] fixed float number (#139)

--- src/pages/acceptDiscount/AcceptDiscount.tsx
+++ src/pages/acceptDiscount/AcceptDiscount.tsx
@@ -111,7 +111,7 @@ const AcceptDiscount = () => {
         const response = await previewPayment({
           productGtin: formData.product!.gtinCode!,
           productName: formData.product!.productName!,
-          amountCents: Number(formData.totalAmount.replace(",", ".")) * 100,
+          amountCents: Math.round(Number(formData.totalAmount.replace(",", ".")) * 100),
           discountCode: formData.discountCode.trim()!,
         });
         sessionStorage.setItem(
