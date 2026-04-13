00c28ee5e827fe34ff58b1dc6be86d9acd51998d
fragreco01 <fragreco@emeal.nttdata.com>
2026-04-13 11:40:20 +0200

    fix: axios

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,7 @@ const IS_DEVELOP = import.meta.env.DEV;
 export const MOCK_USER = IS_DEVELOP;
 export const LOG_REDUX_ACTIONS = IS_DEVELOP;
 
-export const DEBUG_CONSOLE = IS_DEVELOP;
+export const DEBUG_CONSOLE = false;
 
 export const MISSING_DATA_PLACEHOLDER = '-';
 export const REQUIRED_FIELD_ERROR = 'Campo obbligatorio';
