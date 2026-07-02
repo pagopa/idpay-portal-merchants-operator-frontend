6262fd0042dfefcbafe92476e4235dbb37a7c4a4
EleonoraC96 <cossu.eleonora1996@gmail.com>
2026-07-02 16:23:46 +0200

    fix: [BND-80] fixed AuthContext

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState, useMemo, useCallback } from 'react';
+import React, { createContext, useContext, useEffect, useState, useMemo, useCallback, useRef } from 'react';
 import keycloak from '../config/keycloak';
 import type { ReactNode } from 'react';
 import type { JwtUser } from '../utils/types';
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 interface AuthContextType {
   isAuthenticated: boolean;
-  user: JwtUser;
+  user: JwtUser | null;
   token: string | null;
   login: () => void;
   logout: () => void;
@@ -26,14 +26,33 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [token, setToken] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
 
+  const isInitialized = useRef(false)
+
   const setJwtToken = authStore((state) => state.setJwtToken);
   const setLogout = authStore((state) => state.setLogout);
 
+  const login = useCallback(() => {
+    keycloak.login({
+      redirectUri: window.location.origin + '/esercente/', //return to homepage after login
+    });
+  }, []);
+
+  const logout = useCallback(() => {
+    keycloak.logout({
+      redirectUri: window.location.origin + '/esercente/', //return to homepage after logout
+    });
+    setIsAuthenticated(false);
+    setUser(null);
+    setToken(null);
+  }, [setIsAuthenticated, setUser, setToken]);
+
   useEffect(() => {
+    if (isInitialized.current) return;
+    isInitialized.current = true;
     const initKeycloak = async () => {
       try {
         const authenticated = await keycloak.init({
-          onLoad: 'check-sso',
+          onLoad: 'login-required',
           checkLoginIframe: false,
           pkceMethod: 'S256',
         });
@@ -43,8 +62,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
           setToken(keycloak.token || null);
           try {
             const response = await axios.get(
-              `${keycloak.authServerUrl}/realms/${
-                import.meta.env.VITE_KEYCLOAK_REALM
+              `${keycloak.authServerUrl}/realms/${import.meta.env.VITE_KEYCLOAK_REALM
               }/protocol/openid-connect/userinfo`,
               {
                 headers: {
@@ -83,21 +101,6 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 
     return () => clearInterval(interval);
   }, []);
-
-  const login = useCallback(() => {
-    keycloak.login({
-      redirectUri: window.location.origin + '/esercente/', //return to homepage after login
-    });
-  }, []);
-
-  const logout = useCallback(() => {
-    keycloak.logout({
-      redirectUri: window.location.origin + '/esercente/', //return to homepage after logout
-    });
-    setIsAuthenticated(false);
-    setUser(null);
-    setToken(null);
-  }, [setIsAuthenticated, setUser, setToken]);
 
   useEffect(() => {
     setJwtToken(token);
