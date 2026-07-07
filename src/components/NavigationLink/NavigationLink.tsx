a5fc37ffedb7fb0b9216d2f245d060e6f2bfe6fa
EleonoraC96 <cossu.eleonora1996@gmail.com>
2026-07-07 10:41:07 +0200

    chore: fixed underline and test

--- src/components/NavigationLink/NavigationLink.tsx
+++ src/components/NavigationLink/NavigationLink.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom"
 export const NavigationLink = ({ label, path }: { label: string, path: string }) => {
     const navigate = useNavigate()
     return <Tooltip title={label}>
-        <Button sx={{textDecoration: 'underline', ":hover": {textDecoration: 'underline'}}} variant='text' onClick={() => navigate(path, { replace: true })}>
+        <Button variant='text' onClick={() => navigate(path, { replace: true })}>
             {label}
         </Button>
     </Tooltip>
