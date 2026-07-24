8f8940f2b4aa4cff7d22e1721a97604da1d0c92a
EleonoraC96 <cossu.eleonora1996@gmail.com>
2026-07-24 15:06:34 +0200

    fix: [BND-1514]

--- src/components/FileUploadAction/FileUploadAction.tsx
+++ src/components/FileUploadAction/FileUploadAction.tsx
@@ -54,7 +54,7 @@ const FileUploadAction: React.FC<FileUploadActionProps> = ({
   }>();
   const [file, setFile] = useState<File | null>(null);
   const [requiredFileError, setRequiredFileError] = useState<boolean>(false);
-  const [docNumber, setDocNumber] = useState<string>(fileDocNumber || '');
+  const [docNumber, setDocNumber] = useState<string>('');
   const [docNumberError, setDocNumberError] = useState<boolean>(false);
   const [fileSizeError, setFileSizeError] = useState<boolean>(false);
   const [fileTypeError, setFileTypeError] = useState<boolean>(false);
@@ -72,6 +72,13 @@ const FileUploadAction: React.FC<FileUploadActionProps> = ({
       return () => clearTimeout(timer);
     }
   }, [errorAlert]);
+
+  useEffect(() => {
+    if (fileDocNumber) {
+      const decripted = atob(fileDocNumber)
+      setDocNumber(decripted !== 'undefined' ? decripted : '')
+    }
+  }, [fileDocNumber])
 
   const handleFileSelect = (selectedFile: File) => {
     if (selectedFile) {
